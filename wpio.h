4c120dd0a63037b5ae42ae40d03a19c9c3ec1ede
Wudi <wudicgi@gmail.com>
2012-10-26 04:08:36 +0800

    修改 wpio.h 中函数原型的注释

--- wpio.h
+++ wpio.h
@@ -30,35 +30,35 @@ WPIO_Stream *wpio_alloc(const WPIO_StreamOps *ops, void *aux);
 /**
  * 测试流是否支持读取
  *
- * @return bool 若支持读取则返回 true, 否则返回 false
+ * @return bool 若支持读取则返回非 0 值, 否则返回 0
  */
 WPIO_API int wpio_is_readable(WPIO_Stream *stream);
 
 /**
  * 测试流是否支持写入
  *
- * @return bool 若支持写入则返回 true, 否则返回 false
+ * @return bool 若支持写入则返回非 0 值, 否则返回 0
  */
 WPIO_API int wpio_is_writable(WPIO_Stream *stream);
 
 /**
  * 测试流是否支持定位
  *
- * @return bool 若支持定位则返回 true, 否则返回 false
+ * @return bool 若支持定位则返回非 0 值, 否则返回 0
  */
 WPIO_API int wpio_is_seekable(WPIO_Stream *stream);
 
 /**
  * 关闭流
  *
- * @return bool 成功时返回 true, 失败时抛出异常
+ * @return bool 成功时返回 0 (包括 stream 为 NULL 或者未打开), 失败时返回 EOF
  */
 WPIO_API int wpio_close(WPIO_Stream *stream);
 
 /**
  * 将缓冲内容输出到流
  *
- * @return bool 成功时返回 true, 失败时抛出异常
+ * @return bool 成功时返回 0, 失败时返回 EOF
  */
 WPIO_API int wpio_flush(WPIO_Stream *stream);
 
@@ -68,21 +68,21 @@ WPIO_API int wpio_flush(WPIO_Stream *stream);
  * @param int offset   偏移量
  * @param int whence   定位起始点
  *
- * @return bool 成功时返回 true, 失败时抛出异常
+ * @return bool 成功时返回 0, 失败时返回 EOF
  */
 WPIO_API int wpio_seek(WPIO_Stream *stream, off64_t offset, int whence);
 
 /**
  * 获取指针在流中的位置
  *
- * @return int 返回指针在流中的位置，产生错误时抛出异常
+ * @return int 返回指针在流中的位置，产生错误时返回 -1
  */
 WPIO_API off64_t wpio_tell(WPIO_Stream *stream);
 
 /**
  * 测试指针是否到了流结束的位置
  *
- * @return bool 如果指针到了流结束的位置则返回 true, 否则返回 false
+ * @return bool 如果指针到了流结束的位置则返回非 0 值, 否则返回 0
  */
 WPIO_API int wpio_eof(WPIO_Stream *stream);
 
@@ -94,7 +94,7 @@ WPIO_API int wpio_eof(WPIO_Stream *stream);
  *
  * @param int length   最多读取的字节数
  *
- * @return string 返回实际所读取的数据，产生错误时抛出异常
+ * @return string 返回实际所读取的数据
  */
 WPIO_API size_t wpio_read(WPIO_Stream *stream, void *buffer, size_t length);
 
@@ -105,7 +105,7 @@ WPIO_API size_t wpio_read(WPIO_Stream *stream, void *buffer, size_t length);
  *
  * @param string data  要写入的数据
  *
- * @return int 返回实际写入的字节数，产生错误时抛出异常
+ * @return int 返回实际写入的字节数
  */
 WPIO_API size_t wpio_write(WPIO_Stream *stream, const void *buffer, size_t length);
 
