5edbe7a51f197e6a4fd0018bee9cc57b8c5b72e9
Wudi <wudicgi@gmail.com>
2012-10-25 22:00:12 +0800

    将 wpio.c 中函数的注释移动到了 wpio.h 中

--- wpio.c
+++ wpio.c
@@ -28,80 +28,34 @@ WPIO_API int wpio_is_seekable(WPIO_Stream *stream) {
     return 1;
 }
 
-/**
- * 读取流
- *
- * 如果可读取的数据没有请求的那么多，则返回的字符串长度小于所请求的字节数。
- * 如果到达流的结尾，返回值为空字符串。
- *
- * @param int $length   最多读取的字节数
- *
- * @return string 返回实际所读取的数据，产生错误时抛出异常
- */
-WPIO_API size_t wpio_read(WPIO_Stream *stream, void *buffer, size_t length) {
-    return stream->ops->read(stream, buffer, length);
+WPIO_API int wpio_close(WPIO_Stream *stream) {
+    return stream->ops->close(stream);
 }
 
-/**
- * 写入流
- *
- * 如果可写入流的数据没有请求的那么多，则返回的实际写入的字节数小于所请求的字节数。
- *
- * @param string $data  要写入的数据
- *
- * @return int 返回实际写入的字节数，产生错误时抛出异常
- */
-WPIO_API size_t wpio_write(WPIO_Stream *stream, const void *buffer, size_t length) {
-    return stream->ops->write(stream, buffer, length);
-}
-
-/**
- * 将缓冲内容输出到流
- *
- * @return bool 成功时返回 true, 失败时抛出异常
- */
 WPIO_API int wpio_flush(WPIO_Stream *stream) {
     return stream->ops->flush(stream);
 }
 
-/**
- * 在流中定位
- *
- * @param int $offset   偏移量
- * @param int $whence   定位起始点
- *
- * @return bool 成功时返回 true, 失败时抛出异常
- */
 WPIO_API int wpio_seek(WPIO_Stream *stream, off64_t offset, int whence) {
     return stream->ops->seek(stream, offset, whence);
 }
 
-/**
- * 获取指针在流中的位置
- *
- * @return int 返回指针在流中的位置，产生错误时抛出异常
- */
 WPIO_API off64_t wpio_tell(WPIO_Stream *stream) {
     return stream->ops->tell(stream);
 }
 
-/**
- * 测试指针是否到了流结束的位置
- *
- * @return bool 如果指针到了流结束的位置则返回 true, 否则返回 false
- */
+WPIO_API size_t wpio_read(WPIO_Stream *stream, void *buffer, size_t length) {
+    return stream->ops->read(stream, buffer, length);
+}
+
+WPIO_API size_t wpio_write(WPIO_Stream *stream, const void *buffer, size_t length) {
+    return stream->ops->write(stream, buffer, length);
+}
+
 WPIO_API int wpio_eof(WPIO_Stream *stream) {
     return stream->ops->eof(stream);
 }
 
-/**
- * 关闭流
- *
- * @return bool 成功时返回 true, 失败时抛出异常
- */
-WPIO_API int wpio_close(WPIO_Stream *stream) {
-    return stream->ops->close(stream);
-}
 /*
 void main(void) {
     printf("sizeof(size_t) = %d\r\n", sizeof(size_t));
--- wpio.h
+++ wpio.h
@@ -56,16 +56,86 @@ struct _WPIO_Stream {
 
 WPIO_Stream *wpio_alloc(const WPIO_StreamOps *ops, void *aux);
 
+/**
+ * 测试流是否支持读取
+ *
+ * @return bool 若支持读取则返回 true, 否则返回 false
+ */
 WPIO_API int wpio_is_readable(WPIO_Stream *stream);
+
+/**
+ * 测试流是否支持写入
+ *
+ * @return bool 若支持写入则返回 true, 否则返回 false
+ */
 WPIO_API int wpio_is_writable(WPIO_Stream *stream);
+
+/**
+ * 测试流是否支持定位
+ *
+ * @return bool 若支持定位则返回 true, 否则返回 false
+ */
 WPIO_API int wpio_is_seekable(WPIO_Stream *stream);
 
-WPIO_API size_t wpio_read(WPIO_Stream *stream, void *buffer, size_t length);
-WPIO_API size_t wpio_write(WPIO_Stream *stream, const void *buffer, size_t length);
-WPIO_API int wpio_flush(WPIO_Stream *stream);
-WPIO_API int wpio_seek(WPIO_Stream *stream, off64_t offset, int whence);
-WPIO_API off64_t wpio_tell(WPIO_Stream *stream);
-WPIO_API int wpio_eof(WPIO_Stream *stream);
+/**
+ * 关闭流
+ *
+ * @return bool 成功时返回 true, 失败时抛出异常
+ */
 WPIO_API int wpio_close(WPIO_Stream *stream);
+
+/**
+ * 将缓冲内容输出到流
+ *
+ * @return bool 成功时返回 true, 失败时抛出异常
+ */
+WPIO_API int wpio_flush(WPIO_Stream *stream);
+
+/**
+ * 在流中定位
+ *
+ * @param int offset   偏移量
+ * @param int whence   定位起始点
+ *
+ * @return bool 成功时返回 true, 失败时抛出异常
+ */
+WPIO_API int wpio_seek(WPIO_Stream *stream, off64_t offset, int whence);
+
+/**
+ * 获取指针在流中的位置
+ *
+ * @return int 返回指针在流中的位置，产生错误时抛出异常
+ */
+WPIO_API off64_t wpio_tell(WPIO_Stream *stream);
+
+/**
+ * 读取流
+ *
+ * 如果可读取的数据没有请求的那么多，则返回的字符串长度小于所请求的字节数。
+ * 如果到达流的结尾，返回值为空字符串。
+ *
+ * @param int length   最多读取的字节数
+ *
+ * @return string 返回实际所读取的数据，产生错误时抛出异常
+ */
+WPIO_API size_t wpio_read(WPIO_Stream *stream, void *buffer, size_t length);
+
+/**
+ * 写入流
+ *
+ * 如果可写入流的数据没有请求的那么多，则返回的实际写入的字节数小于所请求的字节数。
+ *
+ * @param string data  要写入的数据
+ *
+ * @return int 返回实际写入的字节数，产生错误时抛出异常
+ */
+WPIO_API size_t wpio_write(WPIO_Stream *stream, const void *buffer, size_t length);
+
+/**
+ * 测试指针是否到了流结束的位置
+ *
+ * @return bool 如果指针到了流结束的位置则返回 true, 否则返回 false
+ */
+WPIO_API int wpio_eof(WPIO_Stream *stream);
 
 #endif // __WPIO_H__
